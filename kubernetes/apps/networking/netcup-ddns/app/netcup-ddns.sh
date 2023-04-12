26e6a9356524e62ab77ce6734591362bff5c474d
Alb Dev <alb@MacBook-Pro-von-ALB.local>
2023-04-12 14:58:20 +0200

    feature(netcup-ddns): new ddns for second domain

--- kubernetes/apps/networking/netcup-ddns/app/netcup-ddns.sh
+++ kubernetes/apps/networking/netcup-ddns/app/netcup-ddns.sh
@@ -26,7 +26,7 @@ if [[ $(echo "$EXISTING_RECORD" | grep -c "was not found on the server") -eq 1 ]
   -d "<?xml version=\"1.0\" encoding=\"UTF-8\"?><soap:Envelope xmlns:soap=\"http://www.w3.org/2003/05/soap-envelope\"><soap:Body><domain_dns_zone_record_add xmlns=\"http://webservices.domainapi.com/\"><domain_dns_zone_record><zoneId>$DOMAIN_ID</zoneId><hostname>$RECORD_NAME</hostname><recordtype>$RECORD_TYPE</recordtype><recorddata>$IPV4</recorddata><ttl>120</ttl></domain_dns_zone_record></domain_dns_zone_record_add></soap:Body></soap:Envelope>"
 else
   # Update the existing A record with the new IPv4 address
-  RECORD_ID=$(echo "$EXISTING_RECORD" | grep -oPm1 "(?<=<id>)[^<]+")
+  RECORD_ID=$(echo "$EXISTING_RECORD" | sed -n 's/.*<id>\([^<]*\)<\/id>.*/\1/p')
   echo "Updating A record $RECORD_ID with IP address $IPV4..."
  curl -s -X POST "https://ccp.netcup.net/run/webservice/servers/any/webspace/record?webspace=active" \
  -H "Content-Type: text/xml;charset=UTF-8" \
