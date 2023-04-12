97a6d11ae6e81ac72b9e873394a53b845d728dd0
Alb Dev <alb@MacBook-Pro-von-ALB.local>
2023-04-12 14:41:45 +0200

    feature(netcup-ddns): new ddns for second domain

--- kubernetes/apps/networking/netcup-ddns/app/netcup-ddns.sh
+++ kubernetes/apps/networking/netcup-ddns/app/netcup-ddns.sh
@@ -8,13 +8,14 @@ DOMAIN_ID=$(curl -s -X POST "https://ccp.netcup.net/run/webservice/servers/any/*
 -H "Content-Type: text/xml;charset=UTF-8" \
 -H "Accept: text/xml" \
 -d "<?xml version=\"1.0\" encoding=\"UTF-8\"?><soap:Envelope xmlns:soap=\"http://www.w3.org/2003/05/soap-envelope\"><soap:Body><domain_get_by_name xmlns=\"http://webservices.domainapi.com/\">$DOMAIN</domain_get_by_name></soap:Body></soap:Envelope>" \
-| grep -oPm1 "(?<=<id>)[^<]+")
+| sed -n 's/.*<id>\([^<]*\)<\/id>.*/\1/p')
 
 # Check if the A record exists
 EXISTING_RECORD=$(curl -s -X POST "https://ccp.netcup.net/run/webservice/servers/any/webspace/record?webspace=active" \
 -H "Content-Type: text/xml;charset=UTF-8" \
 -H "Accept: text/xml" \
--d "<?xml version=\"1.0\" encoding=\"UTF-8\"?><soap:Envelope xmlns:soap=\"http://www.w3.org/2003/05/soap-envelope\"><soap:Body><domain_dns_zone_record_info xmlns=\"http://webservices.domainapi.com/\"><domain_dns_zone_record><id></id><zoneId>$DOMAIN_ID</zoneId><hostname>$RECORD_NAME</hostname><recordtype>$RECORD_TYPE</recordtype></domain_dns_zone_record></domain_dns_zone_record_info></soap:Body></soap:Envelope>")
+-d "<?xml version=\"1.0\" encoding=\"UTF-8\"?><soap:Envelope xmlns:soap=\"http://www.w3.org/2003/05/soap-envelope\"><soap:Body><domain_dns_zone_record_info xmlns=\"http://webservices.domainapi.com/\"><domain_dns_zone_record><id></id><zoneId>$DOMAIN_ID</zoneId><hostname>$RECORD_NAME</hostname><recordtype>$RECORD_TYPE</recordtype></domain_dns_zone_record></domain_dns_zone_record_info></soap:Body></soap:Envelope>" \
+| sed -n 's/.*<id>\([^<]*\)<\/id>.*/\1/p')
 
 # If the A record does not exist, create it with the current IPv4 address
 if [[ $(echo "$EXISTING_RECORD" | grep -c "was not found on the server") -eq 1 ]]; then
